fa4ce82fb2e4e8d3eb8054cb6d3a8c5d9648c926
Yareli Cerda <yarelic.1002@gmail.com>
2024-04-10 13:46:06 -0400

    WIP

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//DOM - document object model
+//OBJECT.METHOD parameters( EVENT_NAME, HANDLER_FUNCTION)
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    extra_special.innerHTML = "Replacement Text";
+    extra_special.style.color ="red"; 
+
+}); //END DOM CONTENT LOADED
